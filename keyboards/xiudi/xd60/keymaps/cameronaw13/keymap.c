4df487a2739099cb992ce5909c7da1c7f149bb98
Cameron <cameronawichman@gmail.com>
2024-03-22 01:34:54 -0700

    some final touchups

--- keyboards/xiudi/xd60/keymaps/cameronaw13/keymap.c
+++ keyboards/xiudi/xd60/keymaps/cameronaw13/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,            KC_RBRC,  KC_BSLS,
         TT(2),    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            _____,    KC_ENT,
         KC_LSFT,  _____,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  _____,
-        KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,                       KC_BSPC,            KC_RALT,  TT(2),    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PSCR
+        KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,                       KC_BSPC,            KC_RALT,  TT(2),    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_APP
     ),
     // Base Colemak-DH Layer
     [1] = LAYOUT_60_ansi_split_bs_rshift_space(
@@ -22,15 +22,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,            KC_RBRC,  KC_BSLS,
         TT(2),    KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            _____,    KC_ENT,
         KC_LSFT,  _____,    KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    KC_RSFT,  _____,
-        KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,                       KC_BSPC,            KC_RALT,  TT(2),    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PSCR
+        KC_LCTL,  KC_LGUI,  KC_LALT,            KC_SPC,                       KC_BSPC,            KC_RALT,  TT(2),    KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_APP
     ),
     // Function Layer
     [2] = LAYOUT_60_ansi_split_bs_rshift_space(
-        KC_NUM,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PAUS,  KC_CAPS,
-        vvvvv,    KC_P7,    KC_P8,    KC_P9,    KC_PMNS,  KC_PSLS,  _____,    KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_MPLY,  KC_MPRV,            KC_MNXT,  _____,
+        KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PAUS,  KC_SCRL,
+        KC_NUM,   KC_P7,    KC_P8,    KC_P9,    KC_PMNS,  KC_PSLS,  KC_BTN3,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_MPLY,  KC_MPRV,            KC_MNXT,  _____,
         vvvvv,    KC_P4,    KC_P5,    KC_P6,    KC_PPLS,  KC_PAST,  _____,    KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  KC_WH_U,            _____,    KC_INS,
         vvvvv,    _____,    KC_P1,    KC_P2,    KC_P3,    KC_P0,    KC_PCMM,  _____,    _____,    KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_PGUP,  vvvvv,    _____,
-        vvvvv,    vvvvv,    vvvvv,              KC_ENT,                       KC_DEL,             vvvvv,    vvvvv,    KC_HOME,  KC_PGDN,  KC_END,   _____
+        vvvvv,    vvvvv,    vvvvv,              KC_ENT,                       KC_DEL,             vvvvv,    vvvvv,    KC_HOME,  KC_PGDN,  KC_END,   vvvvv
     ),
     // Extra Layer
     [3] = LAYOUT_60_ansi_split_bs_rshift_space(
@@ -46,7 +46,7 @@ void manage_layer(tap_dance_state_t *state, void *user_data) {
     // One tap for Scroll Lock, two to change default layout, three to enable the extra layer
     switch(state->count) {
         case 1:
-            tap_code(KC_SCRL);
+            tap_code(KC_PSCR);
             break;
         case 2:
             if(layer_state_is(0)) {
@@ -62,7 +62,7 @@ void manage_layer(tap_dance_state_t *state, void *user_data) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-    [MNG_LYR] = ACTION_TAP_DANCE_FN(manage_layer),
+    [MNG_LYR] = ACTION_TAP_DANCE_FN(manage_layer)
 };
 
 uint16_t keycode_config(uint16_t keycode) {
